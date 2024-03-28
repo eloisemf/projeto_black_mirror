6c0f55b1431dd8530551882267feddca69948413
Eloise <113181382+eloisemf@users.noreply.github.com>
2024-03-28 09:10:08 -0300

    Update black_mirror.py

--- black_mirror.py
+++ black_mirror.py
@@ -13,7 +13,6 @@ QUAIS SÃO OS TEMAS PRINCIPAIS EXPLORADOS NESTE EPISÓDIO DE BLACK MIRROR?
 O EPISÓDIO TERMINA DE MANEIRA TÍPICA PARA A SÉRIE BLACK MIRROR? EXPLIQUE.
 '''
 
-pergunta = ""
 
 while  True:
     pergunta = input("O que deseja saber sobre a serie -Joan is awful-?\n")
@@ -23,7 +22,7 @@ while  True:
     elif (pergunta == "Qual o nome do serviço de streaming fictício que parodia a netflix no episódio? "):
         print("\nO nome do serviço de streaming fictício é STREAMBARRY\n")
     elif (pergunta == "Como Joan descobre a existência da série sobre sua vida?"):
-        print("\nJoan descobriu a série indo assistir algo na hora do jantar, seu noivo insiste em ver a serie com uma atriz que se asemelha a ela\n")
+        print("\nJoan descobriu a série indo assistir algo na hora do jantar, seu noivo insiste em ver a serie com uma atriz que se assemelha a ela\n")
     elif (pergunta ==  "Qual a reação inicial de Joan ao descobrir a série e o que ela faz em resposta?"):
         print("\nJoan ficou em choque, teve uma crise de ansiedade e entrou em desespero\n")
     elif (pergunta == "Quais são os temas principais explorados neste episódio de Black Mirror?"):
@@ -34,4 +33,4 @@ while  True:
         print("Tchau. Até a proxima!")
         break
     else: 
-        print("Não tenho conhecimentos o suficiente sobre essa pergunta, lamento.")
+        print("Não tenho conhecimentos suficiente sobre essa pergunta, lamento.")
